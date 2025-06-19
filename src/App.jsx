2b4334ec56de39342fd1e7654f3d6fae02af6862
nod1rDev <nodirbeknegmatov2008@gmail.com>
2025-06-19 16:22:59 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-// import Home from "./components/Home";
+import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 import "./index.css";
-// import About from "./components/About";
+import About from "./components/About";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
 
      
